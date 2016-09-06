59c9be9792aea35f94aebba27d43735198ccfd9c
Fritz Dittmer <fdittmer9762@gmail.com>
2016-09-06 16:12:26 -0600

    Scene Setup, Player Movement
    set up scene and file structure,
    added player movement and jump,
    jump limit and double jump added

---
+++ Star Catcher/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float moveSpeed;
+	public float jumpSpeed;
+	private float jumpLimit = 2;
+	public bool isGrounded = true;
+
+
+	void Start () {
+		isGrounded = true;
+		jumpLimit = 2;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Jump(){
+		rb.AddForce (0, 100 * jumpSpeed * jumpLimit--, 0);
+	}
+
+	void Move () {
+		rb.transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed, 0, 0);
+	}
+
+	void OnTriggerEnter (){
+		jumpLimit = 2;
+		isGrounded = true;
+	}
+
+	void OnTriggerExit() {
+		isGrounded = false;
+	}
+
+	void Update () {
+		Move ();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump();
+		}
+	}
+}
