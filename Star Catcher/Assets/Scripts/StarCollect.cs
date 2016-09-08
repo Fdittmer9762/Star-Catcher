e4a746c4bb7ed9957ecdf7e307fc6115bc6a0f8e
Fritz Dittmer <fdittmer9762@gmail.com>
2016-09-08 14:33:15 -0600

    Stars
    Added star prefab
    Added star collider
    Stars fall when player approaches
    Stars can be collected

---
+++ Star Catcher/Assets/Scripts/StarCollect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarCollect : MonoBehaviour {
+
+	public GameObject star;
+
+	void OnTriggerEnter (){
+		Debug.Log ("Star Collected");
+		star.SetActive (false);
+		//Add Star Points, may do in different script
+	}
+
+}
---
+++ Star Catcher/Assets/Scripts/StarFall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarFall : MonoBehaviour {
+
+	public Vector3 starTraject;
+	public Rigidbody star;
+
+	void Start () {
+		star.useGravity = false;
+		starTraject.x = Random.Range(0f,4f);
+	}
+
+	void OnTriggerEnter () {
+		star.velocity = starTraject;
+		star.useGravity = true;
+	}
+
+}
